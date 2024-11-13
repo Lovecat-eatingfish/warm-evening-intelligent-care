4bb8dc6c881ff4ac5605cb041233f3552969d821
enquan <3278277140@qq.com>
2024-11-13 20:41:25 +0800

    初始化项目

---
+++ warm-stater/src/main/java/com/innovation/warm/WarmApplication.java
@@ -0,0 +1,23 @@
+package com.innovation.warm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Service;
+
+/**
+ * ClassName: ${NAME}
+ * PackageName: com.innovation.warm
+ * Description:
+ *
+ * @Author: 32782
+ * @Date: 2024/11/13 下午7:35
+ * @Version: 1.0
+ */
+//TIP 要<b>运行</b>代码，请按 <shortcut actionId="Run"/> 或
+// 点击装订区域中的 <icon src="AllIcons.Actions.Execute"/> 图标。
+@SpringBootApplication
+public class WarmApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WarmApplication.class, args);
+    }
+}
